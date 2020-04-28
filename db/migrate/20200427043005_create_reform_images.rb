786363ff40b3e75dd4ac178302d54fc66de1883a
yukiktu <yuuki0421ktu@gmail.com>
2020-04-28 13:22:46 +0900

    db:migrate

--- db/migrate/20200427043005_create_reform_images.rb
+++ db/migrate/20200427043005_create_reform_images.rb
@@ -1,7 +1,7 @@
 class CreateReformImages < ActiveRecord::Migration[5.2]
   def change
     create_table :reform_images do |t|
-      #t.references :reform_case, foreign_key: true
+      t.references :reform_case, foreign_key: true
       t.string :befor, default: "error.png"
       t.string :after, default: "error.png"
 
