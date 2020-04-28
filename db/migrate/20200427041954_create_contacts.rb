04163ad937a1b1eeb81e0347f1d9ce9af02591e8
yukiktu <yuuki0421ktu@gmail.com>
2020-04-28 12:14:05 +0900

    修正しました

--- db/migrate/20200427041954_create_contacts.rb
+++ db/migrate/20200427041954_create_contacts.rb
@@ -1,8 +1,8 @@
 class CreateContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
-      t.references :contact_group, foreign_key: true
-      t.references :customer, foreign_key: true
+      #t.references :contact_group, foreign_key: true
+      #t.references :customer, foreign_key: true
       t.text :details
       t.boolean :is_checked, default: false, null: false
 
